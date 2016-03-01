958f8da2fb4bac5f1ef381ecfcda92d71313b1e4
kuilei <kuileishijie@gmail.com>
2016-03-01 19:42:50 +0800

    音量和亮度调节，有些bug，音量和亮度错误

--- app/src/main/java/com/android/tedcoder/androidvideoplayer/MainActivity.java
+++ app/src/main/java/com/android/tedcoder/androidvideoplayer/MainActivity.java
@@ -1,16 +1,22 @@
 package com.android.tedcoder.androidvideoplayer;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+
+import com.android.tedcoder.wkvideoplayer.LightnessController;
 import com.android.tedcoder.wkvideoplayer.util.DensityUtil;
 import com.android.tedcoder.wkvideoplayer.util.ScreenSwitchUtils;
 import com.android.tedcoder.wkvideoplayer.view.MediaController;
 import com.android.tedcoder.wkvideoplayer.view.SuperVideoPlayer;
+import com.android.tedcoder.wkvideoplayer.VolumeController;
 
 /**
  * 演示demo
@@ -19,6 +25,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SuperVideoPlayer mSuperVideoPlayer;
     private View mPlayBtnView;
     private ScreenSwitchUtils instance;
+
+    // 声音调节Toast
+    private VolumeController volumeController;
+
+    // 音频管理器
+    private AudioManager mAudioManager;
+
+    // 屏幕宽高
+    private float width;
+    private float height;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +45,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mPlayBtnView.setOnClickListener(this);
         mSuperVideoPlayer.setVideoPlayCallback(mVideoPlayCallback);
         instance = ScreenSwitchUtils.init(this.getApplicationContext());
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        volumeController = new VolumeController(this);
+        mSuperVideoPlayer.setOnTouchListener(mTouchListener);
+
+        width = DensityUtil.getWidthInPx(this);
+        height = DensityUtil.getHeightInPx(this);
+        threshold = DensityUtil.dip2px(this, 18);
     }
 
 
@@ -87,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mPlayBtnView.setVisibility(View.GONE);
         mSuperVideoPlayer.setVisibility(View.VISIBLE);
         mSuperVideoPlayer.setAutoHideController(false);
-        Uri uri = Uri.parse("http://7xkbzx.com1.z0.glb.clouddn.com/SampleVideo_1080x720_20mb.mp4");
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback_4);
         mSuperVideoPlayer.loadAndPlay(uri,0);
     }
 
@@ -132,5 +157,151 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mSuperVideoPlayer.setPageType(MediaController.PageType.SHRINK);
         }
     }
+    private float mLastMotionX;
+    private float mLastMotionY;
+    private int startX;
+    private int startY;
+    private int threshold;
+    private boolean isClick = true;
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener()
+    {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent)
+        {
+            final float x = motionEvent.getX();
+            final float y = motionEvent.getY();
+
+            switch (motionEvent.getAction())
+            {
+                case MotionEvent.ACTION_DOWN:
+                    mLastMotionX = x;
+                    mLastMotionY = y;
+                    startX = (int)x;
+                    startY = (int)y;
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    float deltaX = x - mLastMotionX;
+                    float deltaY = y -mLastMotionY;
+                    float absDeltaX = Math.abs(deltaX);
+                    float absDeltaY = Math.abs(deltaY);
+
+                    boolean isAdjustAudio = false;
+                    if (absDeltaX > threshold && absDeltaY > threshold)
+                    {
+                        if (absDeltaX < absDeltaY)
+                        {
+                            isAdjustAudio = true;
+                        }
+                        else
+                        {
+                            isAdjustAudio = false;
+                        }
+                    }else if(absDeltaX < threshold && absDeltaY > threshold)
+                    {
+                        isAdjustAudio = true;
+                    }else if (absDeltaX > threshold && absDeltaY > threshold)
+                    {
+                        isAdjustAudio = false;
+                    }else {
+                        return true;
+                    }
+
+                    if (isAdjustAudio)
+                    {
+                        if (x < width/2)
+                        {
+                            if (deltaY > 0)
+                            {
+                                lightDown(absDeltaY);
+                            }
+                            else if (deltaY < 0)
+                            {
+                                lightUp(absDeltaY);
+                            }
+                        }
+                        else
+                        {
+                            if (deltaY > 0)
+                            {
+                                volumeDown(absDeltaY);
+                            }
+                            else if (deltaY < 0)
+                            {
+                                volumeUp(absDeltaY);
+                            }
+                        }
+                    }else
+                    {
+//                        if (deltaX > 0)
+//                        {
+//                            forward(absDeltaX);
+//                        }else if (deltaX < 0)
+//                        {
+//                            backward(absDeltaX);
+//                        }
+                    }
+
+                    mLastMotionX = x;
+                    mLastMotionY = y;
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    if (Math.abs(x - startX) > threshold || Math.abs(y - startY) > threshold)
+                    {
+                        isClick = false;
+                    }
+                    mLastMotionX = 0;
+                    mLastMotionY = 0;
+                    startX = (int)0;
+                    if (isClick)
+                    {
+//                        showOrHide();
+                    }
+                    isClick = true;
+                    break;
+
+                default:
+                    break;
+
+            }
+            return true;
+        }
+    };
+
+
+    private void lightDown(float delatY) {
+        int down = (int) (delatY / height * 255 * 3);
+        int transformatLight = LightnessController.getLightness(this) - down;
+        LightnessController.setLightness(this, transformatLight);
+    }
+
+    private void lightUp(float delatY) {
+        int up = (int) (delatY / height * 255 * 3);
+        int transformatLight = LightnessController.getLightness(this) + up;
+        LightnessController.setLightness(this, transformatLight);
+    }
+
+    private void volumeDown(float delatY) {
+        int max = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int current = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int down = (int) (delatY / height * max * 3);
+        int volume = Math.max(current - down, 0);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+        int transformatVolume = volume * 100 / max;
+        volumeController.show(transformatVolume);
+    }
+
+    private void volumeUp(float delatY) {
+        int max = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int current = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int up = (int) ((delatY / height) * max * 3);
+        int volume = Math.min(current + up, max);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+        int transformatVolume = volume * 100 / max;
+        volumeController.show(transformatVolume);
+    }
+
+
 
 }
